a3d2e14c1441ef1ea0391695ae50ab71a776a00b
Timeless Topics <timelesstopics.online@gmail.com>
2024-08-23 18:04:47 +0530

    Update utils.ts (slugify)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,10 @@
-export function slugify(str: string) {
-    return str.toString()
-        .toLowerCase()
-        .trim()
-        .replace(/\s+/g, "-")
-        .replace(/&/g, "-and-")
-        .replace(/\s*:\s*/g, "-")
-        .replace(/\./g, "")
-        .replace(/\-\-+/g, "-")
+
+
+export function slugify(str:string) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim leading/trailing white space
+  str = str.toLowerCase(); // convert string to lowercase
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
+           .replace(/\s+/g, '-') // replace spaces with hyphens
+           .replace(/-+/g, '-'); // remove consecutive hyphens
+  return str;
 }
